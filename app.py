3d60a731bd37efb948b79dfab9299f70160b701b
ananthakrishnanh <ananthakrishnanh97@gmail.com>
2020-11-15 23:05:04 +0530

    Adding new routes

--- app.py
+++ app.py
@@ -7,6 +7,9 @@ app = Flask(__name__)
 def hello_world():
     return render_template('index.html')
 
+@app.route('/football', methods=['GET'])
+def football():
+    return render_template('football.html')
 
 if __name__ == '__main__':
     app.run(port=4000, debug=True)
